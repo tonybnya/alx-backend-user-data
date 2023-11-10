0b4bd8e580d0cb12fb0a21d6e69f1f0ba07b348e
Tony <nya.tony2010@gmail.com>
2023-11-10 11:40:56 +0100

    Add: 'Basic - Allow password with ' task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,10 @@ class BasicAuth(Auth):
         ):
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.split(':')
+        credentials = decoded_base64_authorization_header.split(':', 1)
+
+        if len(credentials) != 2:
+            return None, None
 
         return credentials[0], credentials[-1]
 
