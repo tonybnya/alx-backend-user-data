0211b4b060cf72e908f33ae52534f77b0db949b6
Tony <nya.tony2010@gmail.com>
2023-11-10 10:37:40 +0100

    Update: 'Basic - Base64 part' task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,16 +7,17 @@ from typing import List, TypeVar
 
 
 class BasicAuth(Auth):
-    """ This class is the template for all authentication system
-    """
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
+    """This class is the template for all authentication system"""
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """Get the Base64 part of the Authorization header
         for a Basic Authentication
         """
-        if authorization_header is None\
-            or not isinstance(authorization_header, str)\
-            or not authorization_header.startswith('Basic '):
+        if (
+            authorization_header is None
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith("Basic ")
+        ):
             return None
 
         return authorization_header[6:]
