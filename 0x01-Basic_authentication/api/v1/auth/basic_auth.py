8aa9bdaff8d7db16ac98957142d739465441a6dc
Tony <nya.tony2010@gmail.com>
2023-11-10 11:30:05 +0100

    Add: 'Basic - Overload current_user - and BOOM' task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -80,3 +80,12 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+
+        return self.user_object_from_credentials(email, password)
