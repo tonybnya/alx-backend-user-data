5a92ef34be32ed13cdc624e77f5f3b8cb465ab00
Tony <nya.tony2010@gmail.com>
2023-11-10 10:18:52 +0100

    Update: 'Basic auth' task

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 """ Class BasicAuth that inherits from Auth
 """
+from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
+
+
+class BasicAuth(Auth):
+    """ This class is the template for all authentication system
+    """
+    pass
