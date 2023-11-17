0f72b861cc40a149e953397e4077eda4c71f2e72
Tony <nya.tony2010@gmail.com>
2023-11-17 10:05:56 +0100

    Update: adding blondeau function that implements the  endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 """Setting up a basic Flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello_world() -> str:
+    """GET /
+    :return: JSON payload of the form {"message": "Bienvenue"}
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["GET"], strict_slashes=False)
+def users() -> str:
+    """POST /users
+    :return: JSON payload with two forms
+    - {"email": "<registered email>", "message": "user created"}
+    - {"message": "email already registered"}
+    """
+    email, pwd = request.form.get("email"), request.form.get("password")
+
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
