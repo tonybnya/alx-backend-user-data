1789148cf9f658aa10fd6902c59ed00e134f2c4b
Tony <nya.tony2010@gmail.com>
2023-11-17 13:58:09 +0100

    Update: 'update_password' method to the 'auth' module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,4 +89,10 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Update password"""
-        pass
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password=password)
+            user.reset_token = None
