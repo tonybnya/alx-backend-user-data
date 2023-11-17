cf502a9948a20684bd938b6960ad2774d337a5c5
Tony <nya.tony2010@gmail.com>
2023-11-17 11:45:45 +0100

    Update: 'get_reset_password_token' method to the 'auth' module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,4 +78,11 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token"""
-        pass
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        user.reset_token = _generate_uuid()
+
+        return user.reset_token
