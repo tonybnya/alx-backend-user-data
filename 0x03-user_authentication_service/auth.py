c36f7c8ed22d033db85fe78a10ff84f0ac2bac85
Tony <nya.tony2010@gmail.com>
2023-11-17 13:54:27 +0100

    Add: 'update_password' method to the 'auth' module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,3 +86,7 @@ class Auth:
         user.reset_token = _generate_uuid()
 
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        pass
