d59dc128904fd7211076c03a9531884bfde3107b
Tony <nya.tony2010@gmail.com>
2023-11-17 07:43:40 +0100

    Update: 'user.py' file

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,12 +10,12 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Class to represent User model and inherits from Base
-    """
-    __tablename__ = 'users'
+    """Class to represent User model and inherits from Base"""
+
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    emaim = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
     reset_token = Column(String(250), nullable=False)
