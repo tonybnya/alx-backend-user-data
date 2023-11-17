cdbf73e82419bb80a86e13973f8c3083958cae6e
Tony <nya.tony2010@gmail.com>
2023-11-17 09:40:01 +0100

    Add: 'app.py' file - a setup for a basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Setting up a basic Flask app
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello_world() -> str:
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
